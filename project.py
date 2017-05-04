82deb188582877f3648257d0e019e8ceea924525
Ashraf Amer <ashraf.amer55@gmail.com>
2017-05-04 07:01:55 -0700

    Update project.py

--- project.py
+++ project.py
@@ -8,12 +8,22 @@ from database_setup import Base, Restaurant, MenuItem
 #New Import
 from flask import session as login_session
 import random, string
+
 #Connect to Database and create database session
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+
+# Create anti-forgery state token
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    return "The current session state is %s" % login_session['state']
 
 
 #JSON APIs to view Restaurant Information
